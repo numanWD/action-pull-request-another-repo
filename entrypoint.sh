5f746aba6dc3ea5df174d70bc00e529b1b28d720
numanWD <diegosevillano@gmail.com>
2023-02-09 21:28:52 +0000

    Fix git push

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,21 +58,22 @@ git add .
 if git status | grep -q "Changes to be committed"
 then
   git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-  echo "Pushing git commit"
-  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
-  echo "Creating a pull request"
   echo $PR_BODY > pr_body
   
   if [ $INPUT_ALLOW_FORCE_PUSH == "false" ]
   then
+    echo "Pushing git commit"
+    git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+    echo "Creating a pull request"
     gh pr create -t "$PR_TITLE" \
                 -F pr_body \
                 -B $INPUT_DESTINATION_BASE_BRANCH \
                 -H $INPUT_DESTINATION_HEAD_BRANCH \
                     $PULL_REQUEST_REVIEWERS
   else
-    echo "Force push enabled"
+    echo "Force git commit"
     git push -uf origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+    echo "Creating a pull request"
     gh pr create -t "$PR_TITLE" \
                 -F pr_body \
                 -B $INPUT_DESTINATION_BASE_BRANCH \
